600915e73f875909b114df530eade9e2b5b1bade
Ed Mackey <elm19087@gmail.com>
2019-02-22 17:36:55 -0500

    Messing with custom node groups

--- addons/io_scene_gltf2/blender/imp/gltf2_blender_map_occlusion.py
+++ addons/io_scene_gltf2/blender/imp/gltf2_blender_map_occlusion.py
@@ -41,3 +41,29 @@ class BlenderOcclusionMap():
             bpy.data.images[gltf.data.images[gltf.data.textures[
                 pymaterial.occlusion_texture.index
             ].source].blender_image_name].use_fake_user = True
+
+
+
+        # Put this definition somewhere common, it will likely change to "glTF Settings" or similar.
+        gltf_node_group_name = 'glTF Metallic Roughness'
+
+        # The following code runs per-material for materials with occlusion.
+
+        # Check if the custom node group already exists
+        if gltf_node_group_name in bpy.data.node_groups:
+            gltf_node_group = bpy.data.node_groups[gltf_node_group_name]
+        else:
+            # Create a new node grouping
+            gltf_node_group = bpy.data.node_groups.new(gltf_node_group_name, 'ShaderNodeTree')
+            gltf_node_group.inputs.new("NodeSocketFloat", "Occlusion")
+            gltf_node_group.nodes.new('NodeGroupOutput')
+            gltf_group_input = gltf_node_group.nodes.new('NodeGroupInput')
+            gltf_group_input.location = -200, 0
+
+        # Place a new node grouping in the current material
+        gltf_settings_node = bpy.data.materials[material_idx].node_tree.nodes.new('ShaderNodeGroup')
+
+        # Set the newly placed node grouping to be the glTF grouping
+        gltf_settings_node.node_tree = gltf_node_group
+        gltf_settings_node.width = 220
+
